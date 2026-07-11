19e3b8e09572f08f34d88f049698a443275bdc98
vrigna <vrignardello@gmail.com>
2026-07-10 22:19:39 -0400

    add functional version

--- terraform/modules/frontend-app/main.tf
+++ terraform/modules/frontend-app/main.tf
@@ -13,7 +13,7 @@ locals {
       phases:
         preBuild:
           commands:
-            - npm ci
+            - npm ci --cache .npm --prefer-offline
         build:
           commands:
             - npm run build
@@ -23,10 +23,12 @@ locals {
           - '**/*'
       cache:
         paths:
-          - node_modules/**/*
           - .next/cache/**/*
+          - .npm/**/*
   YAML
 }
+
+data "aws_caller_identity" "current" {}
 
 data "aws_iam_policy_document" "amplify_assume_role" {
   statement {
@@ -34,30 +36,79 @@ data "aws_iam_policy_document" "amplify_assume_role" {
 
     actions = [
       "sts:AssumeRole",
-      "sts:TagSession",
     ]
 
     principals {
       type = "Service"
       identifiers = [
         "amplify.amazonaws.com",
-        "amplifybackend.amazonaws.com",
-        "codebuild.amazonaws.com",
       ]
     }
   }
 }
 
+data "aws_iam_policy_document" "amplify_ssr_logging" {
+  statement {
+    sid    = "PushLogs"
+    effect = "Allow"
+
+    actions = [
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+    ]
+
+    resources = [
+      "arn:aws:logs:*:${data.aws_caller_identity.current.account_id}:log-group:/aws/amplify/*:log-stream:*",
+    ]
+  }
+
+  statement {
+    sid    = "CreateLogGroup"
+    effect = "Allow"
+
+    actions = [
+      "logs:CreateLogGroup",
+    ]
+
+    resources = [
+      "arn:aws:logs:*:${data.aws_caller_identity.current.account_id}:log-group:/aws/amplify/*",
+    ]
+  }
+
+  statement {
+    sid    = "DescribeLogGroups"
+    effect = "Allow"
+
+    actions = [
+      "logs:DescribeLogGroups",
+    ]
+
+    resources = [
+      "arn:aws:logs:*:${data.aws_caller_identity.current.account_id}:log-group:*",
+    ]
+  }
+}
+
 resource "aws_iam_role" "amplify_service" {
-  name               = "${var.project_name}-amplify-service"
+  name               = "${var.project_name}-amplify-ssr-logging"
+  path               = "/service-role/"
+  description        = "The service role that will be used by AWS Amplify for Web Compute app logging."
   assume_role_policy = data.aws_iam_policy_document.amplify_assume_role.json
+
+  tags = local.common_tags
+}
+
+resource "aws_iam_policy" "amplify_ssr_logging" {
+  name   = "${var.project_name}-amplify-ssr-logging"
+  path   = "/service-role/"
+  policy = data.aws_iam_policy_document.amplify_ssr_logging.json
 
   tags = local.common_tags
 }
 
 resource "aws_iam_role_policy_attachment" "amplify_service" {
   role       = aws_iam_role.amplify_service.name
-  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess-Amplify"
+  policy_arn = aws_iam_policy.amplify_ssr_logging.arn
 }
 
 resource "aws_amplify_app" "frontend" {
@@ -67,12 +118,22 @@ resource "aws_amplify_app" "frontend" {
   platform             = "WEB_COMPUTE"
   iam_service_role_arn = aws_iam_role.amplify_service.arn
 
-  enable_branch_auto_build = true
+  enable_branch_auto_build = false
   build_spec               = local.build_spec
 
   environment_variables = var.environment_variables
 
+  custom_rule {
+    source = "/<*>"
+    target = "/index.html"
+    status = "404-200"
+  }
+
   tags = local.common_tags
+
+  depends_on = [
+    aws_iam_role_policy_attachment.amplify_service,
+  ]
 }
 
 resource "aws_amplify_branch" "environment" {
@@ -81,7 +142,7 @@ resource "aws_amplify_branch" "environment" {
   app_id            = aws_amplify_app.frontend.id
   branch_name       = each.value.branch_name
   display_name      = each.key
-  framework         = "Next.js"
+  framework         = "Next.js - SSR"
   stage             = each.value.stage
   enable_auto_build = true
 
